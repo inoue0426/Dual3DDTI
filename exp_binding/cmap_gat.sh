71ed5e2ae5290c8f59e1a5e75780345b8fef726f
inoue019 <8393063+inoue0426@users.noreply.github.com>
2024-04-07 01:08:30 -0500

    update

---
+++ exp_binding/cmap_gat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l     
+#SBATCH --time=96:00:00
+#SBATCH --mem=400gb
+#SBATCH --requeue
+#SBATCH --job-name='cmap gat'
+#SBATCH --partition a100-4 
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:a100:1
+#SBATCH --mail-type=ALL  
+#SBATCH --mail-user=inoue019@umn.edu 
+
+module load conda
+source activate py39
+python cmap_gat.py
---
+++ exp_binding/pdb_gat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l     
+#SBATCH --time=96:00:00
+#SBATCH --mem=20gb
+#SBATCH --requeue
+#SBATCH --job-name='pdb gcn'
+#SBATCH --partition a100-4 
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:a100:1
+#SBATCH --mail-type=ALL  
+#SBATCH --mail-user=inoue019@umn.edu 
+
+module load conda
+source activate py39
+python pdb_gat.py
